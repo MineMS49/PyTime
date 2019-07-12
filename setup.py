4b8a3165c59958bb901d8ab0759cf8291a2e7ee5
MineMS49 <43464448+MineMS49@users.noreply.github.com>
2019-07-12 09:27:52 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,8 +4,10 @@ import setuptools
 
 
 setuptools.setup(
-    name='PyTime',
-    version='0.1',
-    url='https://github.com/MineMS49/PyTime',
     author='MineMS',
+    name='PyTime',
+    packages=['c14'],
+    setup_requires='setupmeta',
+    url='https://github.com/MineMS49/PyTime',
+    version='0.1'
 )
